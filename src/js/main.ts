19f8a878fbd362b2ea2e93732d022174f3da6e7c
Savus <justin.fuzzygreenstuff@gmail.com>
2024-06-03 00:25:39 -0600

    build logic so that each individual block adds "show" class to classList onClick and then removes "show" class from classList after wait promise resolves

--- src/js/main.ts
+++ src/js/main.ts
@@ -1,9 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
+  type ActiveGuesses = [null | number, null | number];
   type Status = "guessing" | "guessed-correctly" | "blank";
   type Square = {
     color: string;
     status: Status;
   };
+
+  let guesses = [null, null] as ActiveGuesses;
 
   const wait = (s: number) =>
     new Promise((resolve) => {
@@ -13,6 +16,12 @@ document.addEventListener("DOMContentLoaded", () => {
   const $board = document.querySelector(".board-game");
 
   if (!$board) throw new Error("could not find board game");
+
+  $board.addEventListener("click", () => {
+    if (guesses[0] && guesses[1]) {
+      guesses = [null, null];
+    }
+  });
 
   const squares: Square[] = [
     { color: "blue", status: "blank" },
@@ -25,23 +34,34 @@ document.addEventListener("DOMContentLoaded", () => {
     { color: "green", status: "blank" },
   ];
 
-  const buildBlock = ({ color }: Square) => {
+  const buildBlock = ({ color }: Square, index: number) => {
     const $block = document.createElement("div");
     $block.classList.add("block", `bg-${color}`);
     $block.addEventListener("click", function (e) {
       e.stopPropagation();
-      this.classList.add("show");
-      makeAllBlocksTransparent();
+      showBlock(index, true);
+      wait(3).then(() => {
+        showBlock(index, false);
+      });
     });
 
     return $block;
   };
 
   const populateBoardWithBlocks = () =>
-    squares.forEach((square) => {
-      const $block = buildBlock(square);
+    squares.forEach((square, index) => {
+      const $block = buildBlock(square, index);
       $board.append($block);
     });
+
+  const showBlock = (index: number, show: boolean) => {
+    const $blocks = [...document.querySelectorAll(".block")];
+    if (show) {
+      $blocks[index].classList.add("show");
+    } else {
+      $blocks[index].classList.remove("show");
+    }
+  };
 
   const makeAllBlocksTransparent = () => {
     wait(3).then(() => {
