4388a4b06368c65923ef3590a44ecfbdf913d3e7
Savus <justin.fuzzygreenstuff@gmail.com>
2024-06-03 00:45:06 -0600

    bulid logic for determining if user clicks two matching colors among the squares

--- src/js/main.ts
+++ src/js/main.ts
@@ -13,17 +13,17 @@ document.addEventListener("DOMContentLoaded", () => {
       return setTimeout(resolve, s * 1000);
     });
 
-  const $board = document.querySelector(".board-game");
+  const $gameBoard = document.querySelector(".board-game");
 
-  if (!$board) throw new Error("could not find board game");
+  if (!$gameBoard) throw new Error("could not find board game");
 
-  $board.addEventListener("click", () => {
+  $gameBoard.addEventListener("click", () => {
     if (guesses[0] && guesses[1]) {
       guesses = [null, null];
     }
   });
 
-  const squares: Square[] = [
+  const board: Square[] = [
     { color: "blue", status: "blank" },
     { color: "blue", status: "blank" },
     { color: "purple", status: "blank" },
@@ -34,27 +34,50 @@ document.addEventListener("DOMContentLoaded", () => {
     { color: "green", status: "blank" },
   ];
 
+  const isCorrect = () => {
+    const guess1 = guesses[0];
+    const guess2 = guesses[1];
+    if (guess1 === null) throw new Error("no guess 1");
+    if (guess2 === null) throw new Error("no guess 2");
+    return board[guess1].color === board[guess2].color;
+  };
+
   const buildBlock = ({ color }: Square, index: number) => {
     const $block = document.createElement("div");
     $block.classList.add("block", `bg-${color}`);
     $block.addEventListener("click", function (e) {
       e.stopPropagation();
-      showBlock(index, true);
-      wait(3).then(() => {
-        showBlock(index, false);
-      });
+      if (!guesses[0]) {
+        guesses = [index, null];
+        showBlock(index, true);
+        return;
+      }
+      if (!guesses[1]) {
+        guesses[1] = index;
+        showBlock(index, true);
+        if (isCorrect()) {
+          return;
+        } else {
+          alert("You fucked up");
+          wait(1).then(() => {
+            guesses = [null, null];
+            showBlock(guesses[0], false);
+            showBlock(guesses[1], false);
+          });
+        }
+      }
     });
 
     return $block;
   };
 
   const populateBoardWithBlocks = () =>
-    squares.forEach((square, index) => {
+    board.forEach((square, index) => {
       const $block = buildBlock(square, index);
-      $board.append($block);
+      $gameBoard.append($block);
     });
 
-  const showBlock = (index: number, show: boolean) => {
+  const showBlock = (index: number | null, show: boolean) => {
     const $blocks = [...document.querySelectorAll(".block")];
     if (show) {
       $blocks[index].classList.add("show");
